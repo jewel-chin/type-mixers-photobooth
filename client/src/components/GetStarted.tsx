acb5539b7d7f46414d5477e6cca81e4a80d4fa55
jewel-chin <jchin024@e.ntu.edu.sg>
2025-12-20 17:33:36 +0800

    change button to a heart

--- client/src/components/GetStarted.tsx
+++ client/src/components/GetStarted.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Webcam from "react-webcam";
 import TypeMixersLogo from "../assets/type_logo.png";
+import heart from "../assets/heart.png";
 import { startCaptureSequence } from "../helpers/helpers";
 
 type Props = {
@@ -20,7 +21,9 @@ export const GetStarted = ({
     <div className="text-center flex flex-col items-center">
       <img src={TypeMixersLogo} alt="type mixers logo" className="w-48" />
       <span className="text-xs italic mb-4">Type Mixers Photo Booth</span>
+
       <button
+        className="relative flex items-center justify-center cursor-pointer"
         onClick={() =>
           startCaptureSequence({
             setIsStarted,
@@ -29,9 +32,13 @@ export const GetStarted = ({
             setCurrentCount,
           })
         }
-        className="base-button"
       >
-        Start
+        {/* The Heart Image */}
+        <img src={heart} alt="heart" className="w-14 h-14 object-contain" />
+        {/* The Text Overlay - Added flex and centering */}
+        <div className="absolute inset-0 flex items-center justify-center text-cream text-sm pb-1">
+          Start
+        </div>
       </button>
     </div>
   );
