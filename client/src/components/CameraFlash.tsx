9ae0032f30ff45e0f739e7b7b7a6657c014ae4ad
jewel-chin <jchin024@e.ntu.edu.sg>
2025-12-20 19:20:05 +0800

    remove unused import

--- client/src/components/CameraFlash.tsx
+++ client/src/components/CameraFlash.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export const CameraFlash = ({ isFlashing }: { isFlashing: boolean }) => {
   const [visible, setVisible] = useState(false);
